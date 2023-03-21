231188f64791bbc59b772ee87639949aca741e12
LincolnMuraguri <lincolnmuraguri93@gmail.com>
2023-03-21 19:55:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+function findMatching(drivers, drivers2) {
+    return drivers.filter(driver=>driver.toLowerCase() === drivers2.toLowerCase());
+}
+
+function fuzzyMatch(drivers, drivers3){
+    return drivers.filter(naming=>naming.startsWith(drivers3));
+}
+function matchName(drivers, name) {
+    return drivers.filter(check=>check.name === name);
+}
